ff2d76136b12448966b0084769256dbcf270c43a
XiaGary <garyxia10@gmail.com>
2024-09-04 23:13:58 +1200

    Completed exercise 5 of TOP TDD exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if(a % 1 !== 0 || b % 1 !== 0 || a < 0 || b < 0 || typeof a == 'string' || typeof b == 'string' || typeof a !== 'number' || typeof b !== 'number'){
+        return 'ERROR'
+    }else if(a > b){
+        for(let i = b; i <= a; i++){
+            sum += i
+        }
+    } else if (a < b){
+        for(let i = a; i <= b; i++){
+            sum += i
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
