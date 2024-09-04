2338b0d6554295a0276c8a1e9ac19fa4df822566
XiaGary <garyxia10@gmail.com>
2024-09-04 19:20:17 +1200

    Completed exercise 4 of TOP TDD exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+// const arr = [1, 2, 3, 4]
+const removeFromArray = function(arr, ...remove) {
+    for(let i = 0; i < arr.length; i++){
+        for(let rem of remove){
+            if(arr[i] === rem){
+                arr.splice(i, 1)
+                i--
+                break;
+            }
+        }    
+    }
+    return arr;
 };
 
 // Do not edit below this line
