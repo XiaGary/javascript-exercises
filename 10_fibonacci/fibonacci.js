9ba4b85bae660a20427b1bbc2a12d1026cb2ecd3
XiaGary <garyxia10@gmail.com>
2024-09-14 22:08:39 +1200

    Completed exercise 10 of TDD from TOP

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+//1, 1, 2, 3, 5, 8, etc.
 
+const fibonacci = function(num) {
+    const nums = [1,1];
+    if(num == 0){
+        return 0;
+    } else if(num < 0){
+        return 'OOPS'
+    } else if(num > nums.length){
+    for(let i = nums.length; i < num; i++){
+        nums.push(nums[nums.length-1] + nums[nums.length-2]);
+    }
+}
+return nums[num-1];
 };
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
+
