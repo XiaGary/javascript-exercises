592cb8bcf0c4c3d9b07949ea2a27689cdc4889e6
XiaGary <garyxia10@gmail.com>
2024-09-14 20:23:26 +1200

    Completed exercise 8 of TDD of TOP

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
+const add = function(a,b, ...c) {
+	return a + b;
+};
+
+const subtract = function(a, b, ...c) {
+	return a-b-c
+};
+
+const sum = function(arr) {
+    let sum = 0
+    for(let i = 0; i < arr.length; i++){
+      sum += arr[i];
+    }
+    return sum;
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(arr) {
+  let sum = 1;
+  for(let i = 0; i < arr.length; i++){
+    sum *= arr[i];
+  }
+  return sum;
 };
 
-const sum = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  if(num == 0){
+    return 1;
+  } else{
+      for(let i = num; i >= 1; i--){
+    factorial *= i;
+  }
+}return factorial;
 };
 
 // Do not edit below this line
