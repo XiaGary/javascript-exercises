ee20502bbed8df06bc3fa246309f839af4dc4e52
XiaGary <garyxia10@gmail.com>
2024-09-14 20:51:35 +1200

    Completed exercise 9 of TDD from TOP

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let string = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').replaceAll(' ', '');
+    let palindromeCheck = string.split('').reverse().join('');
+    if(string.toLowerCase() == palindromeCheck.toLowerCase()){
+        return true;
+    } else{
+        return false;
+    }
 };
 
 // Do not edit below this line
